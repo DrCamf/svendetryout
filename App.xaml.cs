a1500f420cd0c0f3a2fea948a7b3e568bbcab514
Thorbjørn Hedegaard <drcamf@gmail.com>
2022-08-24 11:37:42 +0200

    firstupload
    testing git ignore setting

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace svendetryout;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace svendetryout;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
